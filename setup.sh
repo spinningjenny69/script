602ddaf6204203913a81acc5efb670fc894564f5
spinningjenny69 <75646815+spinningjenny69@users.noreply.github.com>
2024-08-19 21:53:05 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,23 +4,9 @@
 install_docker() {
   echo "Installing Docker..."
 
-  # Add Docker's official GPG key and repository
-  sudo apt-get update
-  sudo apt-get install -y ca-certificates curl gnupg
-
-  sudo install -m 0755 -d /etc/apt/keyrings
-  sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-  sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-  # Add the Docker repository to apt sources
-  echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-  # Update package list and install Docker
-  sudo apt-get update
-  sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  curl -sSL https://get.docker.com/ | CHANNEL=stable sh
+# After the installation process is finished, you may need to enable the service and make sure it is started (e.g. CentOS 7)
+  systemctl enable --now docker
 
   echo "Docker installed successfully."
 }
@@ -81,7 +67,7 @@ cd ..
 
 # Part 3: Setup OpenProject
 git clone https://github.com/opf/openproject-deploy --depth=1 --branch=stable/14 openproject
-cd openproject
+cd openproject/compose
 
 # Pull the OpenProject Docker image and start containers
 docker-compose pull
